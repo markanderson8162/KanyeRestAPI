dcb5669a28efbcc17f353188706900bfe70836fb
Mark Anderson <markanderson8162@gmail.com>
2021-07-13 19:43:53 -0500

    finished first exercise

---
+++ KanyeRest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeRest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for(int i= 0; i < 5; i++)
+			{
+				QuoteGenerator.RonQuote();
+				QuoteGenerator.KanyeQuote();
+			}
+
+
+		}
+	}
+}
